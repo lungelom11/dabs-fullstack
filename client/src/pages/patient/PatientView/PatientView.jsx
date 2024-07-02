10c1f7cc42cc9fe70cd51b77419d8c7f8b3a3e1c
Lungelo <lungelomkhwemte11@gmail.com>
2024-07-02 22:25:18 +0200

    View Appointment UI

--- client/src/pages/patient/PatientView/PatientView.jsx
+++ client/src/pages/patient/PatientView/PatientView.jsx
@@ -1,5 +1,48 @@
+import "./patientview.css";
+import { Button, Stack } from "@chakra-ui/react";
+
 const PatientView = () => {
-  return <div>PatientView</div>;
+  return (
+    <>
+      <div className="view-appointment-container">
+        <div className="appointment-data-container">
+          <div className="header">
+            <h2>P79332234</h2>
+          </div>
+          <div className="appointment-data">
+            <p>
+              <span className="data">Appointment ID:</span>
+            </p>
+            <p>
+              <span className="data">Date:</span>
+            </p>
+            <p>
+              <span className="data">Time:</span>
+            </p>
+            <p>
+              <span className="data">Reason:</span>
+            </p>
+            <p>
+              <span className="data">Status:</span>
+            </p>
+            <p>
+              <span className="data">Notes:</span>
+            </p>
+          </div>
+          <div className="buttons">
+            <Stack spacing={5}>
+              <Button colorScheme="green" variant="outline">
+                Edit Appointment
+              </Button>
+              <Button colorScheme="red" variant="outline">
+                Cancel Appointment
+              </Button>
+            </Stack>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PatientView;
