706e1ad055971dafaa00603591a940c217f4fbcf
LungeloM <lungelomkhwemte11@gmail.com>
2024-07-22 13:21:18 +0200

    Updated Bookappointment Calendar

--- client/src/components/Calendar/BookCalendar.jsx
+++ client/src/components/Calendar/BookCalendar.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import "./calendar.css";
-import { DateCalendar } from "@mui/x-date-pickers";
+import { DateCalendar  } from "@mui/x-date-pickers";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import dayjs from "dayjs";
+
 
 const theme = createTheme({
   typography: {
@@ -13,14 +16,29 @@ const theme = createTheme({
 });
 
 const BookCalendar = ({appointment_date,setAppointmentDate}) => {
+
+  const [date, setDate] = useState("__")
+
+  const handleDateChange = (newValue) => {
+    setAppointmentDate(newValue)
+    setDate(dayjs(newValue).format("DD MMMM"))
+  }
   
   return (
     <div className="calendar">
       <ThemeProvider theme={theme}>
+        <div style={{display:"flex",justifyContent:"center", alignItems:"center"}}>
+        <h3>{date}</h3>
         <DateCalendar
           value={appointment_date}
-          onChange={(newValue) => setAppointmentDate(newValue)}
-        />
+          defaultValue={appointment_date}
+          onChange={(newValue) => handleDateChange(newValue)}
+          orientation="landscape" 
+          disablePast = {true}
+          reduceAnimations ={true}
+          
+          />
+        </div>
       </ThemeProvider>
     </div>
   );
